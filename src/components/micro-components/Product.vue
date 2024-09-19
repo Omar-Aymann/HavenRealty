583e2571a0c2a028e686a8a9db45e2649607ac94
omar ayman <zeroomar973@gmail.com>
2024-09-20 02:12:51 +0300

    edit product image

--- src/components/micro-components/Product.vue
+++ src/components/micro-components/Product.vue
@@ -47,7 +47,7 @@ onMounted(() => {
 <template>
   <div ref="product" class="product lg:w-1/3 w-full overflow-hidden flex items-center justify-center border-2 border-slate-200 rounded-2xl p-3 flex-col gap-3">
     <div class="aspect-square w-full overflow-hidden rounded-xl">
-      <img :src="props.img" class="w-64 h-full object-top object-cover scale-150" alt="" />
+      <img :src="props.img" class="w-full h-full object-top object-cover scale-150" alt="" />
     </div>
     <div class="content flex flex-col gap-4">
       <div class="flex flex-col gap-1">
